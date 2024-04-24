6bfc9bdcd6cf5cdb7d6a4f78981d9da342016d05
Alexander Scheibner <44095787+Glasgesicht@users.noreply.github.com>
2024-04-24 21:39:47 +0200

    Update member.ts

--- src/config/member.ts
+++ src/config/member.ts
@@ -1,7 +1,63 @@
 export const rnlaf313members: readonly { callsign: string; tailnr: number }[] =
   [
     {
+      callsign: "Redacted",
+      tailnr: 882,
+    },
+    {
+      callsign: "Deadpool",
+      tailnr: 883,
+    },
+    {
+      callsign: "Clutch",
+      tailnr: 884,
+    },
+    {
+      callsign: "Blaine",
+      tailnr: 887,
+    },
+    {
+      callsign: "Prophet",
+      tailnr: 890,
+    },
+    {
+      callsign: "Tommy",
+      tailnr: 891,
+    },
+    {
+      callsign: "Cutstalk",
+      tailnr: 893,
+    },
+    {
       callsign: "Espere",
       tailnr: 894,
+    },
+    {
+      callsign: "Brook",
+      tailnr: 896,
+    },
+    {
+      callsign: "Skysheykh",
+      tailnr: 897,
+    },
+    {
+      callsign: "Rotti",
+      tailnr: 781,
+    },
+    {
+      callsign: "Python",
+      tailnr: 782,
+    },
+    {
+      callsign: "Duka",
+      tailnr: 784,
+    },
+    {
+      callsign: "DJTaz",
+      tailnr: 785,
+    },
+    {
+      callsign: "Knappy",
+      tailnr: 786,
     },
   ];
