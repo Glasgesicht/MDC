aa6d98978883ab92e0a6d0a5c6c40c5257d6aed4
Espere <bernhardt.scheibner@gmail.com>
2024-06-30 17:10:24 +0200

    example uses grid for inner component

--- src/mdcpages/examplePage/childComponent.vue
+++ src/mdcpages/examplePage/childComponent.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        <div v-for="index in new Array(6)">
+    <div class="childGridLayout">
+        <div v-for="index in new Array(6)" class="mcd-s-1 mcd-c-1">
             <a class="border">this is the inside of a child component</a>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>
 a {
     white-space: break-spaces;
+}
+
+.childGridLayout {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr)
 }
 </style>
--- src/mdcpages/examplePage/examplePage.vue
+++ src/mdcpages/examplePage/examplePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mcdpage">
-        <childComponent style="grid-column: 10 / span 6; grid-row: 12 /span 5;" />
+        <childComponent style="grid-column: 10 / span 12; grid-row: 12 /span 10;" />
     </div>
 </template>
 <script setup lang="ts">
