6fc8cf7a76432b30e305cff9525715dcaa03aa41
Alexander Scheibner <44095787+Glasgesicht@users.noreply.github.com>
2024-08-08 23:53:28 +0200

    Add Package Settings Options, CSS refactor to flex

--- src/mdcpages/Settings/packageSettings.vue
+++ src/mdcpages/Settings/packageSettings.vue
@@ -1,101 +1,132 @@
 <template>
   <div class="parent" v-if="file">
-    <p class="">Selected Package</p>
-    <p class="">Package Name</p>
-    <!--<select v-model="selectedPKG">
+    <div style="min-width: 100%" class="parent">
+      <div class="item" style="padding-right: 15px">
+        <p class="">Selected Package</p>
+        <!--<select v-model="selectedPKG">
           <option v-for="pkg in packages">{{ pkg.name }}</option>
         </select>-->
-    <Dropdown
-      v-model="selectedPKG"
-      :options="packages"
-      optionLabel="name"
-      placeholder="Select A Package"
-      class=""
-      style="grid-row: 2"
-    />
-    <Input
-      style="grid-row: 2; padding-left: 4px"
-      class=""
-      v-model="selectedPKG.name"
-    ></Input>
+        <Dropdown
+          v-model="selectedPKG"
+          :options="packages"
+          optionLabel="name"
+          placeholder="Select A Package"
+          class=""
+        />
+      </div>
+      <div style="padding-right: 15px">
+        <p class="">Package Name</p>
+        <Input class="" v-model="selectedPKG.name"></Input>
+      </div>
+      <div>
+        <p>RAMROD (selected)</p>
+        <Dropdown
+          style="grid-row: 2 / span 1; grid-column: 4 / span 2"
+          :options="ramrods"
+          v-model="ramrod"
+          editable
+          class=""
+        />
+      </div>
+    </div>
 
-    <p style="grid-row: 1 / span 1; grid-column: 4 / span 2" class="">
-      RAMROD (selected)
-    </p>
-    <Dropdown
-      style="grid-row: 2 / span 1; grid-column: 4 / span 2"
-      :options="ramrods"
-      v-model="ramrod"
-      editable
-      class=""
-    />
+    <div>
+      <p style="grid-row: 4 / span 1; grid-column: 1 / span 2" class="">
+        Order Flights In Package
+      </p>
 
-    <p style="grid-row: 4 / span 1; grid-column: 1 / span 2" class="">
-      Order Flights In Package
-    </p>
-
-    <DataTable
-      class=""
-      :value="allFlightsFromPackage"
-      showGridlines
-      edit-mode="cell"
-      @rowReorder="onRowReorder"
-      style="grid-row: 5 / span 10; grid-column: 1 / span 7"
-    >
-      <Column rowReorder headerStyle="width: 4rem" :reorderableColumn="false" />
-      <Column header="FLIGHT" headerStyle="width: 10rem"
-        ><template #body="{ index }">FLIGHT #{{ index + 1 }}</template></Column
+      <DataTable
+        class=""
+        :value="allFlightsFromPackage"
+        showGridlines
+        edit-mode="cell"
+        @rowReorder="onRowReorder"
+        style="grid-row: 5 / span 10; grid-column: 1 / span 7"
       >
-      <Column field="callsign" header="Callsign" key="callsign">
-        <template #body="props" style="text-align: unset; align-items: start"
-          >{{ props.data.callsign }} {{ props.data.callsignNumber }}</template
+        <Column
+          rowReorder
+          headerStyle="width: 4rem"
+          :reorderableColumn="false"
+        />
+        <Column header="FLIGHT" headerStyle="width: 10rem"
+          ><template #body="{ index }"
+            >FLIGHT #{{ index + 1 }}</template
+          ></Column
         >
-      </Column>
-      <Column headerStyle="width: 6rem" header="Type" field="aircrafttype">
-        <template #editor="{ index }">
-          <Input v-model="selectedPKG.flights[index].aircrafttype"
-        /></template>
-      </Column>
-      <Column headerStyle="width: 8rem" header="PRI" field="comms.radio1">
-        <template #body="{ index }">
-          {{ selectedPKG.flights[index].mycomm.pri.name }}
-          {{ selectedPKG.flights[index].mycomm.pri.number || "" }}
-          <br />{{ selectedPKG.flights[index].mycomm.pri.freq }}
-        </template></Column
-      >
-      <Column headerStyle="width: 8rem" header="SEC" field="comms.radio2">
-        <template #body="{ index }">
-          {{ selectedPKG.flights[index].mycomm.sec.name }}
-          {{ selectedPKG.flights[index].mycomm.sec.number || "" }}<br />{{
-            selectedPKG.flights[index].mycomm.sec.freq
-          }}</template
-        ></Column
-      >
-      <Column header="FLIGHTLEAD">
-        <template #body="{ data }">
-          {{ data.units[0]?.callsign }}
-        </template></Column
-      >
-      <Column headerStyle="width: 4.3rem">
-        <template #body="{ index }"
-          ><Button
-            icon="pi pi-trash"
-            severity="danger"
-            @click="confirmDelete(index)"
-            outlined
-        /></template>
-      </Column>
-    </DataTable>
+        <Column field="callsign" header="Callsign" key="callsign">
+          <template #body="props" style="text-align: unset; align-items: start"
+            >{{ props.data.callsign }} {{ props.data.callsignNumber }}</template
+          >
+        </Column>
+        <Column headerStyle="width: 6rem" header="Type" field="aircrafttype">
+          <template #editor="{ index }">
+            <Input v-model="selectedPKG.flights[index].aircrafttype"
+          /></template>
+        </Column>
+        <Column headerStyle="width: 8rem" header="PRI" field="comms.radio1">
+          <template #body="{ index }">
+            {{ selectedPKG.flights[index].mycomm.pri.name }}
+            {{ selectedPKG.flights[index].mycomm.pri.number || "" }}
+            <br />{{ selectedPKG.flights[index].mycomm.pri.freq }}
+          </template></Column
+        >
+        <Column headerStyle="width: 8rem" header="SEC" field="comms.radio2">
+          <template #body="{ index }">
+            {{ selectedPKG.flights[index].mycomm.sec.name }}
+            {{ selectedPKG.flights[index].mycomm.sec.number || "" }}<br />{{
+              selectedPKG.flights[index].mycomm.sec.freq
+            }}</template
+          ></Column
+        >
+        <Column header="FLIGHTLEAD">
+          <template #body="{ data }">
+            {{ data.units[0]?.callsign }}
+          </template></Column
+        >
+        <Column headerStyle="width: 4.3rem">
+          <template #body="{ index }"
+            ><Button
+              icon="pi pi-trash"
+              severity="danger"
+              @click="confirmDelete(index)"
+              outlined
+          /></template>
+        </Column>
+      </DataTable>
+    </div>
+    <div>
+      <p style="grid-row: 15 / span 1; grid-column: 1" class="">Situation</p>
 
-    <p style="grid-row: 15 / span 1; grid-column: 1" class="">Situation</p>
+      <TextArea
+        style="min-width: 700px"
+        v-model="selectedPKG.situation"
+        :draggable="false"
+        rows="5"
+        class=""
+      />
+    </div>
+    <div>
+      <p style="min-width: 400px" class="">Aerial Threats</p>
 
-    <TextArea
-      style="grid-row: 16 / span 20; grid-column: 1 / span 7"
-      v-model="selectedPKG.situation"
-      :draggable="false"
-      rows="5"
-      class=""
-    />
+      <TextArea
+        style="min-width: 400px"
+        v-model="selectedPKG.airThreat"
+        :draggable="false"
+        rows="3"
+        class=""
+      />
+    </div>
+    <div>
+      <p style="min-width: 400px" class="">Surface Threats</p>
+
+      <TextArea
+        style="min-width: 400px"
+        v-model="selectedPKG.surfaceThreat"
+        :draggable="false"
+        rows="3"
+        class=""
+      />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -133,9 +164,13 @@ const { file } = storeToRefs(useGlobalStore());
   margin: 0 0;
 }
 .parent {
-  display: grid;
-  grid-template-columns: repeat(8, 1fr);
-  text-align: left;
-  gap: 2px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  /* Align items horizontally at the start */
+}
+.parent > div {
+  padding-right: 15px;
+  padding-bottom: 5px;
 }
 </style>
