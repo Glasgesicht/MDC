3f3328e54d9679b61b815c081633c7c39190f11a
Alexander Scheibner <44095787+Glasgesicht@users.noreply.github.com>
2024-10-07 00:46:03 +0200

    Added Bullseye exports

--- src/config/bullseye.ts
+++ src/config/bullseye.ts
@@ -4,7 +4,7 @@
  *  */
 
 export const bullseyes: { name: string; location: string }[] = [
-  { name: "BASIN", location: "N 33°50.000 // E 036°49.000" },
-  { name: "DRIFT", location: "N 37°24.000 // E 033°09.000" },
-  { name: "CAPE", location: "N 34°34.000 // E 033°02.000" },
+  { name: "BASIN", location: "N 33°50.000’ // E 036°49.000’" },
+  { name: "DRIFT", location: "N 37°24.000’ // E 033°09.000’" },
+  { name: "CAPE", location: "N 34°34.000’ // E 033°02.000’" },
 ];
