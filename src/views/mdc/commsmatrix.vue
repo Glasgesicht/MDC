280c4355933581909fa75ac3c52c12bb07a99b95
Alexander Scheibner <44095787+Glasgesicht@users.noreply.github.com>
2024-11-04 17:08:52 +0100

    Flight Frequency Matrix

--- src/views/mdc/commsmatrix.vue
+++ src/views/mdc/commsmatrix.vue
@@ -5,30 +5,33 @@ import { commTables } from "@/config/frequencies";
 <template>
   <div class="bdr mdcpage" name="mdcpage">
     <div class="c36 r bdr ctr">NATO SECRET - SHRED AFTER USE</div>
-
-    <!-- Header Row with Numbering -->
-    <div class="c36 child">
-      <div class="bdr ctr" v-for="i in new Array(11).keys()" :key="i">
-        <div v-if="i > 0" class="c4">{{ i }}</div>
-        <div v-else class="c3">{{ i }}</div>
-      </div>
-    </div>
-
-    <!-- Data Rows -->
-    <div
-      v-for="([row, items], index) in Object.entries(commTables[0])"
-      :key="row"
-      class="c36 child"
+    <a class="c36 bdr ctr"
+      >CJTF-13 Frequency Matrix issue 1 - {{ new Date().toDateString() }}</a
     >
-      <div v-if="index % 8 === 0" class="c36 header-row">
-        <div v-for="i in new Array(11).keys()" :key="i" class="bdr ctr">
-          <div v-if="i > 0" class="c4">{{ i }}</div>
-          <div v-else class="c3">{{ i }}</div>
+    <div class="inner-grid c36 r47">
+      <!-- Header Row with Numbering -->
+      <!-- Data Rows -->
+      <div
+        v-for="([row, items], index) in Object.entries(commTables[0])"
+        :key="row"
+        class="child"
+      >
+        <div v-if="index % 8 === 0" class="c40 child">
+          <div v-for="i in new Array(10).keys()" class="c4 ctr bdr row-header">
+            {{ i || "" }}
+          </div>
         </div>
-      </div>
-      <div class="bdr c4">{{ row }}</div>
-      <div v-for="item of items" :key="item" class="c3 bdr ctr">
-        {{ item }}
+        <div class="bdr c4 ctr" :style="`color:${row};background-color:grey`">
+          {{ row }}
+        </div>
+        <div
+          v-for="item of items"
+          :key="item"
+          class="c4 bdr ctr"
+          :style="index % 2 ? 'background-color:lightgrey' : ''"
+        >
+          {{ item }}
+        </div>
       </div>
     </div>
 
@@ -38,6 +41,19 @@ import { commTables } from "@/config/frequencies";
 
 <style scoped>
 @import "@/assets/newstyle.css";
+
+.inner-grid {
+  display: inline-grid;
+  grid-template-columns: repeat(40, 1fr);
+  grid-template-rows: repeat(36, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+
+.row-header {
+  background-color: darkgrey;
+  color: white;
+}
 
 .font500 {
   font-weight: 500;
